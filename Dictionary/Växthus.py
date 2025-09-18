d67e474d0a2abd6dd1ee45be4e6784672b6bd2a4
Make <max.syrjalainen@edu.prakticum.fi>
2025-09-18 09:23:50 +0300

    Add files via upload

---
+++ Dictionary/Växthus.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+plants = [
+    {
+        "namn": "tomat",
+        "typ": "grönsak",
+        "temperaturintervall": (18, 25),
+        "senaste_vattning": date(2025, 9, 18)
+    },
+    {
+        "namn": "gurka",
+        "typ": "grönsak",
+        "temperaturintervall": (20, 28),
+        "senaste_vattning": date(2025, 9, 18)
+    },
+    {
+        "namn": "jordgubbe",
+        "typ": "bär",
+        "temperaturintervall": (15, 22),
+        "senaste_vattning": date(2025, 9, 18)
+    },
+    {
+        "namn": "eggplanta",
+        "typ": "grönsak",
+        "temperaturintervall": (22, 30),
+        "senaste_vattning": date(2025, 9, 18)
+    }
+]
